4c39ffb17832485e47ceaf590782769cd69be38d
JDavidYY <37605224+JDavidYY@users.noreply.github.com>
2020-11-02 01:05:01 -0500

    Componentes principales Home y routing module

---
+++ chickenAppFront/src/app/home/home-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MenuHomeComponent } from './pages/menu-home/menu-home.component';
+import { NavHomeComponent } from './pages/nav-home/nav-home.component';
+
+
+// se declara los componentes que se usaran en el modulo
+export const HOME_COMPONENTS = [
+    MenuHomeComponent,
+    NavHomeComponent
+];
+
+// se arma el path segun el componente que queremos mostrar
+const homeRoutes:Routes = [
+	{
+        path: "",
+        component: NavHomeComponent,
+        children: [
+          {
+            path: '',
+            component: MenuHomeComponent
+          }    
+        ]
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(homeRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class HomeRoutingModule { }
