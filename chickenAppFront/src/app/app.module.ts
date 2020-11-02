15736372425d9b78dc8d9c2b0cc902360147e0d2
JDavidYY <37605224+JDavidYY@users.noreply.github.com>
2020-11-02 16:23:12 -0500

    upgrade home

--- chickenAppFront/src/app/app.module.ts
+++ chickenAppFront/src/app/app.module.ts
@@ -1,27 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
+import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    //Se importan todos los archivos "Module" de cada carpeta módulo
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
     AppRoutingModule,
-    SharedModule,
-    LoginModule
+    HomeModule,
+    LoginModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
