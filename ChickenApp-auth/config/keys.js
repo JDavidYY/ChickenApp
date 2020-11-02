6d33e54b501072ba54b656dd5d886197e4b553d7
Perla Peralta <perla.peralta@unmsm.edu.pe>
2020-11-02 17:13:44 -0500

    add: setting up different roles

--- ChickenApp-auth/config/keys.js
+++ ChickenApp-auth/config/keys.js
@@ -2,4 +2,5 @@ module.exports = {
   mongoURI:
     "mongodb+srv://admin:admin@chickenapp.b33m3.mongodb.net/auth-db?retryWrites=true&w=majority",
   secretOrKey: "secret",
+  userRoles: ["user", "manager"],
 };
