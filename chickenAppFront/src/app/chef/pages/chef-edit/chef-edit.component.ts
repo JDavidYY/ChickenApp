c7e86ec2192556cb764e78389f9ea46c6c4fbfcb
JDavidYY <37605224+JDavidYY@users.noreply.github.com>
2020-11-17 10:42:33 -0500

    chef-edit module

---
+++ chickenAppFront/src/app/chef/pages/chef-edit/chef-edit.component.ts
@@ -0,0 +1,134 @@
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+import { DatePipe} from '@angular/common';
+import { FormControl, Validators } from '@angular/forms';
+import { ChefModel } from '../../models/chef-info.model';
+import { ChefService } from '../../services/chef.service';
+
+@Component({
+  selector: 'app-chef-edit',
+  templateUrl: './chef-edit.component.html',
+  styleUrls: ['./chef-edit.component.scss']
+})
+export class ChefEditComponent implements OnInit {
+
+ 
+  chef: ChefModel = new ChefModel();
+  nacimiento: string;
+
+  dniFormControl = new FormControl('', [
+  Validators.required,
+  Validators.minLength(8)
+  ]);
+
+  apellidoPatFormControl = new FormControl('', [
+  Validators.required,
+  ]);
+
+  apellidoMatFormControl = new FormControl('', [
+  Validators.required,
+  ]);
+
+  nombresFormControl = new FormControl('', [
+  Validators.required
+  ]);
+
+  fechaNacimientoFormControl = new FormControl('', [
+  Validators.required
+  ]);
+
+  celularFormControl = new FormControl('', [
+  Validators.required,
+  Validators.minLength(9)
+  ]);
+
+  emailFormControl = new FormControl('', [
+  Validators.required,
+  Validators.email
+  ]);
+
+  direccionFormControl = new FormControl('', [
+  Validators.required
+  ]);
+
+
+  constructor(private router:Router, private datePipe: DatePipe, private chefservice: ChefService) { }
+
+  ngOnInit(): void {
+  }
+
+  guardarChef(){
+
+    let selectedDate = this.datePipe.transform(this.nacimiento, 'yyyy/MM/dd');
+    this.chef.nacimiento=selectedDate;
+
+    // validacion de campos para que no sean vacios
+    if ( !this.chef.nombre || !this.chef.apellidoPat || !this.chef.apellidoMat || 
+        !this.chef.celular || !this.chef.direccion || !this.chef.dni ||
+      !this.chef.email  || !this.chef.nacimiento ) {
+
+      return;
+    }
+   // validacion de campos para que no sean incorrectos mediante FormControl
+    if ( this.dniFormControl.invalid || this.nombresFormControl.invalid || this.apellidoPatFormControl.invalid 
+      || this.apellidoMatFormControl.invalid || this.celularFormControl.invalid
+      || this.direccionFormControl.invalid || this.dniFormControl.invalid || this.emailFormControl.invalid
+       || this.fechaNacimientoFormControl.invalid ) {
+      return;
+    }
+  //modal para que muestre el mensaje para confirmación de guardado del chef mientras se hace el servicio
+      Swal.fire({
+      title: 'Aviso',
+      text: "¿Estás seguro que deseas guardar el chef?",
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Si, guardar!' ,
+
+      }).then((result) => {
+      // llamado sel servicio guardarChef desde chef.service.ts y se le pasa 2 parametros
+      if (result.value) {
+        this.chefservice.guardarChef(this.chef)
+        .subscribe(
+          (response) => {
+            console.log(response);
+            if ( response.status && response.statusCode == 200 && response.message != "" )
+              Swal.fire(
+                'Enhorabuena!',
+                'El chef ha sido guardado.',
+                'success'
+                );
+              this.router.navigate(['/chef/list']);
+          },
+          (err) => {
+            console.log(err);
+          }
+        );
+      }
+    });
+  }
+
+  regresar(){
+    this.router.navigate(['/chef/list']);
+  }
+
+  keypressNumbers(event: any) {
+    const pattern = /[0-9]/;
+    const inputChar = String.fromCharCode(event.charCode);
+    if (!pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+    }
+
+  keypressLetters(event: any) {
+    const pattern = /[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]/;
+    const inputChar = String.fromCharCode(event.charCode);
+    if (!pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+    }   
+
+}
