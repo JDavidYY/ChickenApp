0c2bd64d26fd643cfd37bcb7cb2b83ecbbcbcb32
JDavidYY <37605224+JDavidYY@users.noreply.github.com>
2020-11-19 22:16:22 -0500

    deliveryboy module

---
+++ chickenAppFront/src/app/deliveryboy/models/deliveryboy-info.model.ts
@@ -0,0 +1,12 @@
+export class DeliveryboyModel
+{
+    idDeliveryboy:string = "";
+    nombre:string = "";
+    apellidoPat:string = "";
+    apellidoMat:string = "";
+    dni:string = "";
+    nacimiento:string = "";
+    celular:string = "";
+    direccion:string = "";
+    email:string = "";
+}
