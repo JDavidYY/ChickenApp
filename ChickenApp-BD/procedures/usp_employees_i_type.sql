69da5dd31d2b76d68dce7d5ff0e8b826859f8458
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2020-11-23 14:39:29 -0500

    Update usp_employees_i_type.sql

--- ChickenApp-BD/procedures/usp_employees_i_type.sql
+++ ChickenApp-BD/procedures/usp_employees_i_type.sql
@@ -1,9 +1,8 @@
-CREATE PROCEDURE `usp_cliente_i_type`(nameTypeE VARCHAR(45),descriptionTypeEmployees VARCHAR(200),estateTypeEmployees VARCHAR(45))
+CREATE PROCEDURE `usp_employees_i_type`(nameTypeE VARCHAR(45),descriptionTypeEmployees VARCHAR(200),estateTypeEmployees VARCHAR(45), OUT outResult INT)
 BEGIN
 	IF (SELECT exists(SELECT * FROM TypeEmployees WHERE nameTypeEmployees=nameTypeE)) = FALSE THEN
 		INSERT INTO TypeEmployees (nameTypeEmployees,descriptionTypeEmployees,estateTypeEmployees)
 		VALUES ('nameTypeE','descriptionTypeEmployees','estateTypeEmployees') ;
-    ELSE
-		SELECT "Este campo ya existe";
+        SET outResult = 1 ;
 	END IF;
 END
