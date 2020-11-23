0df0c4ffd68287bc5ab191e6a5d2ea794e98f512
chemo235 <julio_gutierrezd@hotmail.com>
2020-11-23 15:13:00 -0500

    Update usp_categories_s_categories.sql

--- ChickenApp-BD/procedures/usp_categories_s_categories.sql
+++ ChickenApp-BD/procedures/usp_categories_s_categories.sql
@@ -1,6 +1,6 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `usp_categories_s_categories`()
 BEGIN
-	if (SELECT EXISTS(select * from categories)) then
-		select idCategories as "idCategory" , nameCategories as "name" , descriptionCategories as "description" from categories where estateCategories=1;
+	if (SELECT EXISTS(select * from Categories)) then
+		select idCategories as "idCategory" , nameCategories as "name" , descriptionCategories as "description" from Categories where estateCategories=1;
     end if;
 END
