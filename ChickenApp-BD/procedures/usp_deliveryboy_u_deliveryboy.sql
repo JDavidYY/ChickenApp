c3223f391aec89645724a6c4c429bc566719c72f
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2020-11-23 16:11:26 -0500

    Update usp_deliveryboy_u_deliveryboy.sql

--- ChickenApp-BD/procedures/usp_deliveryboy_u_deliveryboy.sql
+++ ChickenApp-BD/procedures/usp_deliveryboy_u_deliveryboy.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE `usp_deliveryboy_u_deliveryboy`(pfristname VARCHAR(45),plastname VARCHAR(45),pphone VARCHAR(45),pemail VARCHAR(45), ppassword VARCHAR(45),pdni VARCHAR(20),pworkshift VARCHAR(45),pages VARCHAR(45),OUT oresult INT)
 BEGIN
-	IF (SELECT exists(SELECT * FROM Users WHERE emailUsers = pemail)) = FALSE THEN
+	IF (SELECT exists(SELECT * FROM Users WHERE emailUsers = pemail)) = TRUE THEN
 		UPDATE Employees
         SET
 			firstnameEmployees = 'pfristname' ,
