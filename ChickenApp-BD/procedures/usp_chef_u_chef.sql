abc4f336be21697511dd749844619aa6b22683d2
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2020-11-23 15:34:17 -0500

    Create usp_chef_u_chef.sql

---
+++ ChickenApp-BD/procedures/usp_chef_u_chef.sql
@@ -0,0 +1,25 @@
+CREATE PROCEDURE `usp_chef_u_chef`(pfristname VARCHAR(45),plastname VARCHAR(45),pphone VARCHAR(45),pemail VARCHAR(45), ppassword VARCHAR(45),pdni VARCHAR(20),pworkshift VARCHAR(45),pages VARCHAR(45),OUT oresult INT)
+BEGIN
+	IF (SELECT exists(SELECT * FROM Users WHERE emailUsers = pemail)) = FALSE THEN
+		UPDATE Employees
+        SET
+			firstnameEmployees = 'pfristname' ,
+            lastnameEmployees = 'plastname',
+            phoneEmployees = 'pphone',
+            workshiftEmployees = 'pworkshift' ,
+            dniEmployees = 'pdni',
+            ageEmployees = 'pages'
+            
+        WHERE    
+            Employees.dniEmployees =  pdni ;
+		SET oresult = (SELECT idUsers FROM Users WHERE emailUsers = pemail);
+        UPDATE Users
+        SET
+			emailUsers = 'pemail' ,
+            passwordUsers = 'ppassword'
+        WHERE    
+            idUsers =  oresult ;
+        
+        SET oresult = 1;
+	END IF;
+END
