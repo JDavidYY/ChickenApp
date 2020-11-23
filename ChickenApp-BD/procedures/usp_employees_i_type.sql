f61c70933f52d24bcbb243be76debeb91ce2cdb2
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2020-11-23 17:19:14 -0500

    Update usp_employees_i_type.sql

--- ChickenApp-BD/procedures/usp_employees_i_type.sql
+++ ChickenApp-BD/procedures/usp_employees_i_type.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE `usp_employees_i_type`(nameTypeE VARCHAR(45),descriptionTypeEmp
 BEGIN
 	IF (SELECT exists(SELECT * FROM TypeEmployees WHERE nameTypeEmployees=nameTypeE)) = FALSE THEN
 		INSERT INTO TypeEmployees (nameTypeEmployees,descriptionTypeEmployees,estateTypeEmployees)
-		VALUES ('nameTypeE','descriptionTypeEmployees','estateTypeEmployees') ;
+		VALUES (nameTypeE,descriptionTypeEmployees,estateTypeEmployees) ;
         SET outResult = 1 ;
 	END IF;
 END
