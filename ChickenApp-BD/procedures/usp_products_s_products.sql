b56639b01df767fac3b2d9ddf6477c82b139c289
chemo235 <julio_gutierrezd@hotmail.com>
2020-11-23 15:17:23 -0500

    Update usp_products_s_products.sql

--- ChickenApp-BD/procedures/usp_products_s_products.sql
+++ ChickenApp-BD/procedures/usp_products_s_products.sql
@@ -1,6 +1,6 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `usp_products_s_products`()
 BEGIN
-	if (SELECT EXISTS(select * from products)) then
-		select * from products where estateProducts=1;
+	if (SELECT EXISTS(select * from Products)) then
+		select idProducts as "idProduct" , nameProducts as "name" , descriptionProducts as "description" , priceProducts as "price" , Categories_idCategories as "categoryid" from Products where estateProducts=1;
     end if;
 END
