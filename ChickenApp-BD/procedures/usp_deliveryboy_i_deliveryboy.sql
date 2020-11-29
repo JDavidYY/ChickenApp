81fbe195fdd13dbdd43d786968b2f3bb7b99c5ec
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2020-11-29 01:31:11 -0500

    Update usp_deliveryboy_i_deliveryboy.sql

--- ChickenApp-BD/procedures/usp_deliveryboy_i_deliveryboy.sql
+++ ChickenApp-BD/procedures/usp_deliveryboy_i_deliveryboy.sql
@@ -1,14 +1,13 @@
-CREATE PROCEDURE `usp_deliveryboy_i_deliveryboy`(pfristname VARCHAR(45),plastname VARCHAR(45),pphone VARCHAR(45),pemail VARCHAR(45), ppassword VARCHAR(45),pdni VARCHAR(20),pworkshift VARCHAR(45),pages VARCHAR(45),OUT oresult INT)
+CREATE PROCEDURE `usp_deliveryboy_i_deliveryboy`(pfirstname VARCHAR(45),plastname VARCHAR(45),pphone VARCHAR(45),pemail VARCHAR(45), padress VARCHAR(45), ppassword VARCHAR(45),pdni VARCHAR(20),pworkshift VARCHAR(45),pages VARCHAR(45),OUT oresult INT)
 BEGIN
-	IF (SELECT exists(SELECT * FROM Users WHERE emailUsers=pemail)) = FALSE THEN
-		INSERT INTO Users (idUsers,emailUsers,passwordUsers,typeUser,estateUser)
-		VALUES (NULL,pemail,ppassword,'2',1);
+	
+	IF (SELECT EXISTS(SELECT * FROM Users WHERE emailUsers=pemail)) = FALSE THEN
+		INSERT INTO Users (emailUsers,passwordUsers,typeUser,estateUser)
+		VALUES (pemail,ppassword,'4',1);
         
-		SET oresult = (SELECT MAX(Users.idUsers));
-        
-		INSERT INTO Employees (idEmployees,activityEmployees,firstnameEmployees,lastnameEmployees,phoneEmployees,workshiftEmployees,dniEmployees,ageEmployees,adressEmployees,TypeEmployees_idDepartaments,Users_idUsers,estateEmployees,emailEmployees)
-		VALUES(NULL,1,pfristname,plastname,pphone,pworkshift,pdni,pages,NULL,3,oresult,1,pemail);
+		INSERT INTO Employees (activityEmployees,firstnameEmployees,lastnameEmployees,phoneEmployees,workshiftEmployees,dniEmployees,ageEmployees,adressEmployees,TypeEmployees_idDepartaments,Users_idUsers,estateEmployees,emailEmployees)
+		VALUES(1,pfirstname,plastname,pphone,pworkshift,pdni,pages,padress,3,(SELECT MAX(idUsers) FROM Users),1,pemail);
 		
         SET oresult = 1;
 	END IF;
-END
+END ;
