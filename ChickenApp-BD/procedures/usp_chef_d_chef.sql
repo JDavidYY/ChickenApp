4135cf8b2fb5fb1debc1b12edb76e726d391f567
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2020-11-29 01:03:14 -0500

    Update usp_chef_d_chef.sql

--- ChickenApp-BD/procedures/usp_chef_d_chef.sql
+++ ChickenApp-BD/procedures/usp_chef_d_chef.sql
@@ -1,10 +1,8 @@
-CREATE PROCEDURE `usp_chef_d_chef`(dni VARCHAR(20),OUT oresult INT)
+CREATE PROCEDURE `usp_chef_d_chef`(pidchef VARCHAR(20),OUT oresult INT)
 BEGIN
-	IF (SELECT exists(SELECT * FROM Employees WHERE dniEmployees=dni)) = TRUE THEN
-		UPDATE Employees
-        SET 
-		estateEmployees = 0 
-        WHERE dniEmployees=dni;
+	IF (SELECT exists(SELECT * FROM Employees WHERE idEmployees=pidchef)) = TRUE THEN
+		UPDATE Employees SET estateEmployees = 0 WHERE idEmployees=pidchef;
+		UPDATE Users SET estateUser = 0 WHERE idUsers=(select Users_idUsers from Employees where idEmployees=pidchef);
         SET oresult = 1;
 	END IF;
-END
+END ;;
