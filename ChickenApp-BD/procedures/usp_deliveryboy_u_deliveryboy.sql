79c712c38d9d3d27024e2c0ebfe9fd56c6f66544
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2020-11-29 01:33:42 -0500

    Update usp_deliveryboy_u_deliveryboy.sql

--- ChickenApp-BD/procedures/usp_deliveryboy_u_deliveryboy.sql
+++ ChickenApp-BD/procedures/usp_deliveryboy_u_deliveryboy.sql
@@ -1,25 +1,22 @@
-CREATE PROCEDURE `usp_deliveryboy_u_deliveryboy`(pfristname VARCHAR(45),plastname VARCHAR(45),pphone VARCHAR(45),pemail VARCHAR(45), ppassword VARCHAR(45),pdni VARCHAR(20),pworkshift VARCHAR(45),pages VARCHAR(45),OUT oresult INT)
+CREATE DEFINER=`admin`@`%` PROCEDURE `usp_deliveryboy_u_deliveryboy`(piddeliveryboy  VARCHAR(45), pfirstname VARCHAR(45),plastname VARCHAR(45),pphone VARCHAR(45) ,pemail VARCHAR(50) ,padress VARCHAR(50),pdni VARCHAR(20),pworkshift VARCHAR(45),pages VARCHAR(45),OUT oresult INT)
 BEGIN
-	IF (SELECT exists(SELECT * FROM Users WHERE emailUsers = pemail)) = TRUE THEN
+	IF (SELECT EXISTS(SELECT * FROM Employees WHERE idEmployees = piddeliveryboy)) = TRUE THEN
 		UPDATE Employees
         SET
-			firstnameEmployees = pfristname ,
+			firstnameEmployees = pfirstname ,
             lastnameEmployees = plastname,
+            dniEmployees = pdni,
             phoneEmployees = pphone,
             workshiftEmployees = pworkshift ,
-            dniEmployees = pdni,
-            ageEmployees = pages
+            ageEmployees = pages,
+            emailEmployees=pemail,
+            adressEmployees=padress
             
         WHERE    
-            Employees.dniEmployees =  pdni ;
-		SET oresult = (SELECT idUsers FROM Users WHERE emailUsers = pemail);
-        UPDATE Users
-        SET
-			emailUsers = pemail ,
-            passwordUsers = ppassword
-        WHERE    
-            idUsers =  oresult ;
+             idEmployees = piddeliveryboy;
+
+        UPDATE Users SET emailUsers = pemail  WHERE  idUsers =  (select Users_idUsers from Employees where idEmployees = piddeliveryboy );
         
         SET oresult = 1;
 	END IF;
-END
+END ;
