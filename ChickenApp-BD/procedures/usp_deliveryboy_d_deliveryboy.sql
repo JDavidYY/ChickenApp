70c21861a37307d3f669785a473fc9c1c9de0d61
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2020-11-29 01:19:01 -0500

    Update usp_deliveryboy_d_deliveryboy.sql

--- ChickenApp-BD/procedures/usp_deliveryboy_d_deliveryboy.sql
+++ ChickenApp-BD/procedures/usp_deliveryboy_d_deliveryboy.sql
@@ -1,10 +1,8 @@
-CREATE PROCEDURE `usp_deliveryboy_d_deliveryboy`(dni VARCHAR(20),OUT oresult INT)
+CREATE PROCEDURE `usp_deliveryboy_d_deliveryboy`(piddeleveryboy VARCHAR(20),OUT oresult INT)
 BEGIN
-	IF (SELECT exists(SELECT * FROM Employees WHERE dniEmployees=dni)) = TRUE THEN
-		UPDATE Employees
-        SET 
-		estateEmployees = 0 
-        WHERE dniEmployees=dni;
+	IF (SELECT exists(SELECT * FROM Employees WHERE idEmployees=piddeleveryboy)) = TRUE THEN
+		UPDATE Employees SET estateEmployees = 0 WHERE idEmployees=piddeleveryboy;
+		UPDATE Users SET estateUser = 0 WHERE idUsers=(select Users_idUsers from Employees where idEmployees=piddeleveryboy);
         SET oresult = 1;
 	END IF;
-END
+END ;
