3adaf7522d111eb0516d1ec61a55c9e8be45ba36
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2020-11-29 01:15:18 -0500

    Update usp_user_i_customer.sql

--- ChickenApp-BD/procedures/usp_user_i_customer.sql
+++ ChickenApp-BD/procedures/usp_user_i_customer.sql
@@ -1,14 +1,12 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `usp_user_i_customer`(pfristname VARCHAR(45),plastname VARCHAR(45),pphone VARCHAR(45),pemail VARCHAR(45), padress VARCHAR(100),ppassword VARCHAR(45),OUT oresult INT)
+CREATE PROCEDURE `usp_client_i_client`(IN pfirstname VARCHAR(45),IN plastname VARCHAR(45),IN pphone VARCHAR(45),IN pemail VARCHAR(45),IN padress VARCHAR(100),IN ppassword VARCHAR(45),OUT oresult INT)
 BEGIN
 	IF (SELECT exists(SELECT * FROM Users WHERE emailUsers=pemail)) = FALSE THEN
 		INSERT INTO Users (idUsers,emailUsers,passwordUsers,typeUser,estateUser)
 		VALUES (NULL,pemail,ppassword,'1',1);
         
-		SET oresult = (SELECT MAX(Users.idUsers));
-        
 		INSERT INTO Customers (idCustomers,firstnameCustomers,lastnameCustomers,phoneCustomers,emailCustomers,adressCustomers,Users_idUsers,estateCustomers)
-		VALUES(NULL,pfristname,plastname,pphone,pemail,padress,@oresult,1);
+		VALUES(NULL,pfirstname,plastname,pphone,pemail,padress,(SELECT MAX(idUsers) FROM Users),1);
 		
         SET oresult = 1;
 	END IF;
-END
+END ;
