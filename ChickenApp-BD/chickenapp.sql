51ff34991ef67747e8e2b2f3fd5b022b9a011f17
ItaloKor <73799354+italokor@users.noreply.github.com>
2020-12-07 17:32:14 -0500

    Update chickenapp.sql

--- ChickenApp-BD/chickenapp.sql
+++ ChickenApp-BD/chickenapp.sql
@@ -187,9 +187,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Chickenapp`.`Products_has_Combo`
+-- Table `Chickenapp`.`Products_Combo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Chickenapp`.`Products_has_Combo` (
+CREATE TABLE IF NOT EXISTS `Chickenapp`.`Products_Combo` (
   `Products_idProducts` INT NOT NULL,
   `Combo_idCombos` INT NOT NULL,
   PRIMARY KEY (`Products_idProducts`, `Combo_idCombos`),
