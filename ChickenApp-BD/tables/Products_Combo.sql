dcd5e327dc77be16fb6e7b3f706e059421c017ee
ItaloKor <73799354+italokor@users.noreply.github.com>
2020-12-07 17:32:20 -0500

    Create Products_Combo.sql

---
+++ ChickenApp-BD/tables/Products_Combo.sql
@@ -0,0 +1,18 @@
+-- -----------------------------------------------------
+-- Table `Chickenapp`.`Products_Combo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Chickenapp`.`Products_Combo` (
+  `Products_idProducts` INT NOT NULL,
+  `Combo_idCombos` INT NOT NULL,
+  PRIMARY KEY (`Products_idProducts`, `Combo_idCombos`),
+  CONSTRAINT `fk_Products_has_Combo_Products1`
+    FOREIGN KEY (`Products_idProducts`)
+    REFERENCES `Chickenapp`.`Products` (`idProducts`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Products_has_Combo_Combo1`
+    FOREIGN KEY (`Combo_idCombos`)
+    REFERENCES `Chickenapp`.`Combo` (`idCombos`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
