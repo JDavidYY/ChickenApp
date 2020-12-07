3aca4f801afc4b29e42dd40a1573725c12bb6085
Christianbz <73799229+Christianbz@users.noreply.github.com>
2020-12-06 23:55:48 -0500

    menu añadido - (por completar)

--- chickenAppFront/src/app/app.module.ts
+++ chickenAppFront/src/app/app.module.ts
@@ -11,12 +11,12 @@ import { DeliveryboyModule } from './deliveryboy/deliveryboy.module';
 import { CategoryModule } from './category/category.module';
 //import { ProductModule } from './product/product.module';
 import { ClientModule } from './client/client.module';
-import { PromotionListComponent } from './promotion/pages/promotion-list/promotion-list.component';
-import { MenuComponent } from './purchase/pages/menu/menu.component';
-import { ComboEditComponent } from './combo/pages/combo-edit/combo-edit.component';
-import { ComboListComponent } from './combo/pages/combo-list/combo-list.component';
+//import { PromotionListComponent } from './promotion/pages/promotion-list/promotion-list.component';
+//import { MenuComponent } from './purchase/pages/menu/menu.component';
+//import { ComboEditComponent } from './combo/pages/combo-edit/combo-edit.component';
+//import { ComboListComponent } from './combo/pages/combo-list/combo-list.component';
 import { PurchaseModule } from './purchase/purchase.module';
-import { PromotionModule } from './promotion/promotion.module';
+//import { PromotionModule } from './promotion/promotion.module';
 
 
 @NgModule({
