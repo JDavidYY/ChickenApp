3e90dac0f2cfb51e4eabb6c57c1d86ccf0482990
ItaloKor <73799354+italokor@users.noreply.github.com>
2020-12-14 15:10:00 -0500

    Update Products_Combo.sql

--- ChickenApp-BD/tables/Products_Combo.sql
+++ ChickenApp-BD/tables/Products_Combo.sql
@@ -4,6 +4,7 @@
 CREATE TABLE IF NOT EXISTS `Chickenapp`.`Products_Combo` (
   `Products_idProducts` INT NOT NULL,
   `Combo_idCombos` INT NOT NULL,
+  `discount` INT  NULL,
   PRIMARY KEY (`Products_idProducts`, `Combo_idCombos`),
   CONSTRAINT `fk_Products_has_Combo_Products1`
     FOREIGN KEY (`Products_idProducts`)
