bbfeea093c931d71f7ae1afebb6bce0b153164d9
chemo235 <julio_gutierrezd@hotmail.com>
2020-12-27 18:50:38 -0500

    MVC order

---
+++ chickenAppFront/src/app/order/models/order-info.model.ts
@@ -0,0 +1,13 @@
+export class OrderModel
+{
+    idOrder:string = "";
+    firstname:string = "";
+    lastname:string = "";
+    dni: string = "";
+    phone:string = "";
+    workshift: string = "";
+    age: string = "";
+    email:string = "";
+    password: string = "";
+    adress:string = "";
+}
