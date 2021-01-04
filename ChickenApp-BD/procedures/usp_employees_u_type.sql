efd2cff77bcffad94176c4daac99e217641c7be0
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2021-01-04 17:38:20 -0500

    Create usp_employees_u_type.sql

---
+++ ChickenApp-BD/procedures/usp_employees_u_type.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE `usp_employees_u_type`(IN IdtE INT,nameTypeE VARCHAR(45),descriptionTypeEmployees VARCHAR(200),estateTypeEmployees VARCHAR(45), OUT outResult INT)
+BEGIN
+	IF (SELECT exists(SELECT * FROM TypeEmployees WHERE idTypeEmployees=IdtE)) = TRUE THEN
+		UPDATE TypeEmployees
+		SET nameTypeEmployees= nameTypeE,
+			descriptionTypeEmployees = descriptionTypeEmployees,
+			estateTypeEmployees = estateTypeEmployees 
+		WHERE idTypeEmployees=IdtE;
+        SET outResult = 1 ;
+	END IF;
+END|
