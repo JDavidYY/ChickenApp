fa4f138450ebbc39d5f38f8da6d2ae05f2fdb5a7
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2021-01-04 18:31:43 -0500

    Update usp_client_u_client.sql

--- ChickenApp-BD/procedures/usp_client_u_client.sql
+++ ChickenApp-BD/procedures/usp_client_u_client.sql
@@ -1,7 +1,7 @@
 CREATE DEFINER=`admin`@`%` PROCEDURE `usp_client_u_client`(IN idclient INT,IN pfirstname VARCHAR(45),IN plastname VARCHAR(45),IN pphone VARCHAR(45),IN padress VARCHAR(100),OUT oresult INT)
 BEGIN
 	IF (SELECT EXISTS(SELECT * FROM Users WHERE idCustomers=idclient)) = FALSE THEN
-		UPDATE Users 
+		UPDATE Customers 
 		SET firstnameCustomers = pfirstname,lastnameCustomers = plastname ,phoneCustomers = pphone,adressCustomers = padress
 		WHERE idCustomers=idclient;
         SET oresult = 1;
