cba15979ffccc4dbe0cdb35a95c5c22cf33368ae
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2021-01-04 17:38:16 -0500

    Create usp_chef_d_chef.sql

---
+++ ChickenApp-BD/procedures/usp_chef_d_chef.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE 'usp_chef_d_chef'(pidchef VARCHAR(20),OUT oresult INT)
+BEGIN
+	IF (SELECT EXISTS(SELECT * FROM Employees WHERE idEmployees = pidchef)) = TRUE THEN
+		UPDATE Employees SET estateEmployees = 0 WHERE idEmployees = pidchef;
+		UPDATE Users SET estateUser = 0 WHERE idUsers = (select Users_idUsers from Employees where idEmployees=pidchef);
+        SET oresult = 1;
+	END IF;
+END ;
