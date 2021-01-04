3ba6c124fac9d5a2de44533ae51bda6335fdd914
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2021-01-04 18:13:35 -0500

    Create usp_client_u_client.sql

---
+++ ChickenApp-BD/procedures/usp_client_u_client.sql
@@ -0,0 +1,9 @@
+CREATE DEFINER=`admin`@`%` PROCEDURE `usp_client_u_client`(IN idclient INT,IN pfirstname VARCHAR(45),IN plastname VARCHAR(45),IN pphone VARCHAR(45),IN padress VARCHAR(100),OUT oresult INT)
+BEGIN
+	IF (SELECT EXISTS(SELECT * FROM Users WHERE idCustomers=idclient)) = FALSE THEN
+		UPDATE Users 
+		SET firstnameCustomers = pfirstname,lastnameCustomers = plastname ,phoneCustomers = pphone,adressCustomers = padress
+		WHERE idCustomers=idclient;
+        SET oresult = 1;
+	END IF;
+END
