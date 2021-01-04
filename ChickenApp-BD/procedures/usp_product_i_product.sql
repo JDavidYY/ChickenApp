761a24e433bd20aa32e85f2e6d53fca24e701c8d
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2021-01-04 17:44:10 -0500

    Update usp_product_i_product.sql

--- ChickenApp-BD/procedures/usp_product_i_product.sql
+++ ChickenApp-BD/procedures/usp_product_i_product.sql
@@ -1,7 +1,8 @@
-CREATE DEFINER=`admin`@`%` PROCEDURE `usp_product_i_product`(in pname varchar(45),in pdescription varchar(200),in pprice varchar(150),in  pcategoryid int,out oresult int)
+CREATE DEFINER=`admin`@`%` PROCEDURE `usp_product_i_product`(in pname VARCHAR(45),in pdescription VARCHAR(200),in pprice VARCHAR(150),in  pcategoryid INT,out oresult INT)
 BEGIN
-	if (SELECT EXISTS(select * from Products where nameProducts=pname))=false then
-		insert into Products (nameProducts,descriptionProducts,priceProducts,Categories_idCategories,estateProducts) values (pname,pdescription,pprice,pcategoryid,1);
-		set oresult=1;
-	end if;
+	IF (SELECT EXISTS(select * from Products where nameProducts=pname))=false then
+		INSERT INTO Products (nameProducts,descriptionProducts,priceProducts,Categories_idCategories,estateProducts) 
+		VALUES (pname,pdescription,pprice,pcategoryid,1);
+		SET oresult=1;
+	END IF;
 END
