6655d3a5eddd0deabf332942d37ef922985efc59
Dwightrh <69371974+Dwightrh@users.noreply.github.com>
2021-01-04 17:49:22 -0500

    Update usp_product_u_product.sql

--- ChickenApp-BD/procedures/usp_product_u_product.sql
+++ ChickenApp-BD/procedures/usp_product_u_product.sql
@@ -1,7 +1,9 @@
 CREATE DEFINER=`admin`@`%` PROCEDURE `usp_product_u_product`(in pproductid int,in pname varchar(45),in pdescription varchar(200),in pprice varchar(150),in pcategoryid int,out oresult int)
 BEGIN
-	if (SELECT EXISTS(select * from Products where idProducts=pproductid and estateProducts=1)) then
-		update Products set nameProducts=pname,descriptionProducts=pdescription,priceProducts=pprice,Categories_idCategories=pcategoryid where idProducts=pproductid;
-            set oresult=1;
-	end if;
+	IF (SELECT EXISTS(SELECT  * FROM Products WHERE idProducts=pproductid AND estateProducts=1)) THEN
+		UPDATE Products 
+		SET nameProducts=pname,descriptionProducts=pdescription,priceProducts=pprice,Categories_idCategories=pcategoryid 
+		WHERE idProducts=pproductid;
+        SET oresult=1;
+	END IF;
 END
