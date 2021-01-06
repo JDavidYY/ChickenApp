63c3d3077ef2c7343e801fbfb3f997595c0c70b1
chemo235 <julio_gutierrezd@hotmail.com>
2021-01-05 22:23:41 -0500

    Codigo modo "F"

--- chickenAppFront/src/app/order/pages/order-detail/order-detail.component.spec.ts
+++ chickenAppFront/src/app/order/pages/order-detail/order-detail.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { OrderDetailComponent } from './order-detail.component';
 
 describe('OrderDetailComponent', () => {
