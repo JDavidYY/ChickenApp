925c843af5e5487b1f8ad0004956e394fdadac91
chemo235 <julio_gutierrezd@hotmail.com>
2021-01-23 14:26:59 -0500

    TIK-005

--- chickenAppFront/src/app/deliveryboy/pages/deliveryboy-edit/deliveryboy-edit.component.ts
+++ chickenAppFront/src/app/deliveryboy/pages/deliveryboy-edit/deliveryboy-edit.component.ts
@@ -39,7 +39,9 @@ export class DeliveryboyEditComponent implements OnInit {
 
     ageFormControl = new FormControl('', [
     Validators.required,
-    Validators.minLength(2)
+    Validators.maxLength(2),
+    Validators.min(18),
+    Validators.max(40)
     ]);
 
     workshiftFormControl = new FormControl('', [
